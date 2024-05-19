e57a563a92fbacd1cd9306498dc0aea9134de616
Ajandro <adelaygleisafleitas@gmail.com>
2024-05-18 22:41:52 -0400

    3ro

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by <a href="#">Alejandro de la Yglesia</a>.
       </div>
     </footer>
   );
